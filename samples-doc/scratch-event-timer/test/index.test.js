ed27dcdc3caa58c344ef3af6195759dbd0e7ae5a
Harald Sack <harald.sack@t-systems.com>
2026-03-09 15:12:56 +0100

    tests added

--- samples-doc/scratch-event-timer/test/index.test.js
+++ samples-doc/scratch-event-timer/test/index.test.js
@@ -1,42 +1,24 @@
-const globalAgent = require('global-agent');
-if (!process.env.GLOBAL_AGENT_HTTP_PROXY) {
-  process.env.GLOBAL_AGENT_HTTP_PROXY =
-    process.env.HTTPS_PROXY || process.env.HTTP_PROXY || '';
-}
-process.env.GLOBAL_AGENT_FORCE_GLOBAL_AGENT = 'true';
-globalAgent.bootstrap();
-
 const { randomUUID } = require('crypto');
+
 const { Context } = require("fg-runtime");
-const { getTempAKSK } = require("fg-testutils");
 const { handler } = require("../src/index");
 
-
-const event = {
-  "Key": "test",
-};
+const event = require("../resources/test_event.json");
 
 async function runTest() {
-  const creds = await getTempAKSK();
 
   const context = new Context({
     requestID: randomUUID(),
     funcEnv: {
-      RUNTIME_FUNC_NAME: "sdk-obs",
+      RUNTIME_FUNC_NAME: "timer-test",
       RUNTIME_PACKAGE: "default",
       RUNTIME_PROJECT_ID: process.env.OTC_SDK_PROJECTID,
       RUNTIME_FUNC_VERSION: "latest",
       RUNTIME_MEMORY: "128",
       RUNTIME_CPU: "1",
-      RUNTIME_USERDATA:
-        '{"OBS_ENDPOINT":"https://obs.otc.t-systems.com"}',
       RUNTIME_TIMEOUT: "5",
     },
 
-    securityAccessKey: creds.ak,
-    securitySecretKey: creds.sk,
-    securityToken: creds.token,
-    logger: console,
   });
 
   const callback = (error, result) => {    
