56345533f527ecb2cd0c3922e28b6737cfeb4d59
Harald Sack <harald.sack@t-systems.com>
2026-06-15 08:58:53 +0200

    TF provider updated to 1.36.68

--- samples-doc/container-http-koa/terraform/provider.tf
+++ samples-doc/container-http-koa/terraform/provider.tf
@@ -52,7 +52,7 @@ terraform {
 
     opentelekomcloud = {
       source  = "opentelekomcloud/opentelekomcloud"
-      version = ">= 1.36.57"
+      version = ">= 1.36.68"
     }
   }
   backend "s3" {
